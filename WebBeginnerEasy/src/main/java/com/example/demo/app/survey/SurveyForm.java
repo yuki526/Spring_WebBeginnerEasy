094155f0b66eaf84331dc4ad9dce051e89b85829
yuki526 <yk.g187@gmail.com>
2021-02-11 15:29:05 +0900

    36

--- WebBeginnerEasy/src/main/java/com/example/demo/app/survey/SurveyForm.java
+++ WebBeginnerEasy/src/main/java/com/example/demo/app/survey/SurveyForm.java
@@ -2,22 +2,26 @@ package com.example.demo.app.survey;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class SurveyForm{
 
 	/*Add parameters(0~150) 引数を追加(0~150)*/
+	@Min(value = 0)
+	@Max(value = 150)
     private int age;
-    
+
     /*Add parameters(1~5) 引数を追加(1~5)*/
+	@Min(value = 1)
+	@Max(value = 5)
     private int satisfaction;
 
     /*Add parameters(200 characters or less) 引数を追加(200文字以内)*/
+	@Size(max = 200)
     private String comment;
-	
+
 	public SurveyForm() {};
-	
+
     public SurveyForm(int age, int satisfaction, String comment) {
 		this.age = age;
 		this.satisfaction = satisfaction;
